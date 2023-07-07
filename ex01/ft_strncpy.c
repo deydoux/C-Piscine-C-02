9dd7a70aa85777b96b7308edcc9640cc557c3508
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-07 12:44:22 +0200

    Fix ex01

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 09:37:24 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/07 12:23:35 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/07 12:43:59 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 	while (i < n)
 	{
 		dest[i] = '\0';
