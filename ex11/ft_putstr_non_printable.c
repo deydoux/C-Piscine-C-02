3518a7cb43916917d1c868d92b82365340ddf94b
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-08 14:52:19 +0200

    Add ex11

---
+++ ex11/ft_putstr_non_printable.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 13:45:03 by deydoux           #+#    #+#             */
+/*   Updated: 2023/07/08 14:44:52 by deydoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	puthex(char c)
+{
+	char	*base;
+
+	base = "0123456789abcdef";
+	ft_putchar('\\');
+	ft_putchar(base[c / 16]);
+	ft_putchar(base[c % 16]);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < ' ' || str[i] == 127)
+			puthex(str[i]);
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+}
