448404cfee4623bde46d3f9a95831533c28d3530
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-09 16:27:46 +0200

    Fix ex10: empty dest exception

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 09:37:24 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/09 09:15:51 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/09 16:26:59 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	while (i < size - 1 && src[i] != '\0')
+	while (size != 0 && i < size - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
