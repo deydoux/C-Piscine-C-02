abc5783eb55c3c22b956551c0cc5576b104bbfa3
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-09 09:25:27 +0200

    Fix ex10: prevent size for NUL character

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 09:37:24 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/08 13:15:06 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/09 09:15:51 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	while (i < size && src[i] != '\0')
+	while (i < size - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
