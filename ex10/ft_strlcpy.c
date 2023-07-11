526f201182c7066c5f462b051fb0acf28faa60b4
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-11 16:36:28 +0200

    fix ex10: if statement

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 09:37:24 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/11 16:34:01 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/11 16:36:01 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	i = 0;
 	if (size != 0)
 	{
-		while (&& i < size - 1 && src[i] != '\0')
+		while (i < size - 1 && src[i] != '\0')
 		{
 			dest[i] = src[i];
 			i++;
