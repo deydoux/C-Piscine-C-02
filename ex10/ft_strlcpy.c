f4213fcf9d85c1c0c01e1e9cf22ebac9fa616da0
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-11 16:35:08 +0200

    Fix ex10: code style

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 09:37:24 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/09 16:26:59 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/11 16:34:01 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	while (size != 0 && i < size - 1 && src[i] != '\0')
+	if (size != 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (&& i < size - 1 && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 	while (src[i] != '\0')
 		i++;
 	return (i);
